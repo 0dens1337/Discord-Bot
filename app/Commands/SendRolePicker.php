60c9b6de318f3ab9607dd3b22ddd5023acc6551a
0dens1337 <did.9696@mail.ru>
2025-04-10 20:50:22 +0400

    feature/new-command

--- app/Commands/SendRolePicker.php
+++ app/Commands/SendRolePicker.php
@@ -3,24 +3,26 @@
 namespace App\Commands;
 
 use App\Enums\ColorMessageEnum;
+use App\Enums\EmojiMessageEnum;
 use Discord\Builders\MessageBuilder;
+use Discord\Parts\Interactions\Interaction;
 use Laracord\Commands\Command;
 
-class SendReactionMessage extends Command
+class SendRolePicker extends Command
 {
     /**
      * The command name.
      *
      * @var string
      */
-    protected $name = 'color';
+    protected $name = 'role';
 
     /**
      * The command description.
      *
      * @var string
      */
-    protected $description = 'The Send Reaction Message command.';
+    protected $description = 'The Send Emoji Picker command.';
 
     /**
      * Determines whether the command requires admin permissions.
@@ -43,8 +45,6 @@ class SendReactionMessage extends Command
      * @param  array  $args
      * @return void
      */
-
-
     public function handle($message, $args)
     {
         $guild = $message->guild;
@@ -53,7 +53,7 @@ class SendReactionMessage extends Command
         $requiredRole = config('laracord.role.admin');
         if (! $member->roles->has($requiredRole)) return;
 
-        $roles = $guild->roles->filter(function ($role) {
+        $rolesWithNumbers = $guild->roles->filter(function ($role) {
             return preg_match('/\d/', $role->name);
         })->map(function ($role) {
             return [
@@ -62,32 +62,53 @@ class SendReactionMessage extends Command
             ];
         })->toArray();
 
-        $targetChannelId = config('laracord.messages.role_channel_id');
-        $targetChannel = $guild->channels->get('id', $targetChannelId);
+        $rolesWithIcons = $guild->roles->filter(function ($role) {
+            return $role->icon != null;
+        })->map(function ($role) {
+            return [
+                'label' => (string) $role->name,
+                'value' => (string) $role->id,
+            ];
+        })->toArray();
 
-        if ($targetChannel) {
-            $this
-                ->message(ColorMessageEnum::TEMPLATE->value)
-                ->title('ㅤㅤㅤㅤ')
-                ->select($roles, placeholder: ColorMessageEnum::PICK_A_COLOR->value, route: 'roles')
-                ->send($targetChannel);
-
-            $message->channel->sendMessage(ColorMessageEnum::SUCCESS->value);
+        $this
+            ->message(ColorMessageEnum::TEMPLATE->value)
+            ->title('ㅤㅤㅤㅤ')
+            ->image('')
+            ->select($rolesWithNumbers, placeholder: ColorMessageEnum::PICK_A_COLOR->value, route: 'roles_with_numbers')
+            ->select($rolesWithIcons, placeholder: EmojiMessageEnum::PICK_AN_EMOJI->value, route: 'roles_with_icons')
+            ->send($message);
         }
-    }
 
-    /**
+
+        /**
      * The command interaction routes.
      */
     public function interactions(): array
     {
         return [
-            'roles' => function (\Discord\Parts\Interactions\Interaction $interaction) {
+            'roles_with_numbers' => function (Interaction $interaction) {
                 $roleId = $interaction->data->values[0];
                 $member = $interaction->member;
 
                 $rolesToRemove = $member->roles->filter(function ($role) {
                     return preg_match('/\d/', $role->name);
+                });
+
+                foreach ($rolesToRemove as $role) {
+                    $member->removeRole($role->id);
+                }
+
+                $member->addRole($roleId);
+
+                $interaction->respondWithMessage(MessageBuilder::new()->setContent(ColorMessageEnum::SUCCESS_UPDATED->value), ephemeral: true);
+            },
+            'roles_with_icons' => function (Interaction $interaction) {
+                $roleId = $interaction->data->values[0];
+                $member = $interaction->member;
+
+                $rolesToRemove = $member->roles->filter(function ($role) {
+                    return $role->icon != null;
                 });
 
                 foreach ($rolesToRemove as $role) {
--- app/Enums/ColorMessageEnum.php
+++ app/Enums/ColorMessageEnum.php
@@ -5,7 +5,6 @@ namespace App\Enums;
 enum ColorMessageEnum: string
 {
     case PICK_A_COLOR = 'Можешь выбрать цвет ника на сервере';
-    case SUCCESS = 'Команда с цветами успешно выполнена!';
     case SUCCESS_UPDATED = 'Роль успешно обновлена!';
     case TEMPLATE = 'Текст сообщения';
 }
